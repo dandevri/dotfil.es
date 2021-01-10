19b29ae049be20d9098bff879c0c754a88850a6a
dandevri <hi@dandevri.es>
2021-01-10 20:22:31 +0100

    Add bitbar plugins

---
+++ apps/bitbar/spotrate.10s.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# <bitbar.title>Coinbase.com Spot rate</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Mat Ryer</bitbar.author>
+# <bitbar.author.github>matryer</bitbar.author.github>
+# <bitbar.desc>Shows latest spot rate values (in USD) for Bitcoins in the Coinbase exchange.</bitbar.desc>
+
+echo -n "BTC: €"; curl -s "https://api.coinbase.com/v2/prices/spot?currency=EUR" | egrep -o '"amount":"[0-9]+(\.)?([0-9]{0,2}")?' | sed 's/"amount"://'  | sed 's:^.\(.*\).$:\1:'
